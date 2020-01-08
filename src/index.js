68a35731214589b6504af7927574f1cbf7b7de49
sajo.john@millar-bryce.com <sajo.john@millar-bryce.com>
2020-01-08 11:57:17 +0000

    Routes swtches, Redirection and path params

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import { render } from "react-dom";
 import App from "./components/App";
 
-render(<App />, document.getElementById("root"));
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
