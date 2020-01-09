f171f006aaceeb1cac36ae69cd5c0b5ffa319724
sajo.john@millar-bryce.com <sajo.john@millar-bryce.com>
2020-01-09 15:02:53 +0000

    course completed

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -4,7 +4,7 @@ class AboutPage extends React.Component {
   render() {
     return (
       <>
-        <h2>About</h2> <p> This app uses React</p>
+        <h2>About</h2> <p> This app uses React and Flux</p>
       </>
     );
   }
